539bde58e75accf00c8f732b552069b1538f13f2
Yu Feng <fengyu8299@gmail.com>
2019-10-02 14:32:39 -0700

    lecture2

---
+++ code/verify.rkt
@@ -0,0 +1,27 @@
+#lang rosette
+(require rosette/lib/match rosette/lib/angelic)
+
+(define (poly x)
+ (+ (* x x x x) (* 6 x x x) (* 11 x x) (* 6 x)))
+ 
+(define (factored x)
+ (* x (+ x 1) (+ x 2) (+ x 2)))
+ 
+(define (same p f x)
+ (assert (= (p x) (f x))))
+
+
+;;; (same poly factored 0)
+;;; (same poly factored -1)
+;;; (same poly factored -2)
+
+(define-symbolic i integer?)
+
+(define cex (verify (same poly factored i)))
+(evaluate i cex)
+
+(poly -6)
+
+(factored -6)
+
+(same poly factored -6)
