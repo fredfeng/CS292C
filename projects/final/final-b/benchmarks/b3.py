070629d5e8303d05e36e77a48e68e5094c018688
Yanju Chen <chyanju@gmail.com>
2025-05-31 15:36:39 -0700

    Update b3.py

--- projects/final/final-b/benchmarks/b3.py
+++ projects/final/final-b/benchmarks/b3.py
@@ -1,8 +1,8 @@
 # Elements as (x0,x1,x2), (y0,y1,y2) over Fp²  with U^3 = ξ
 def mul_fp6_naive(x, y, xi):
     # schoolbook: 18 Fp² muls
-    z0 = x0*y0 - (x1*y2 + x2*y1)*xi
-    z1 = x0*y1 + x1*y0 - x2*y2*xi
+    z0 = x0*y0 + (x1*y2 + x2*y1)*xi
+    z1 = x0*y1 + x1*y0 + x2*y2*xi
     z2 = x0*y2 + x1*y1 + x2*y0
     return (z0, z1, z2)
 
